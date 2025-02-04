33b9e192c36317eaeb56b407b4cd937794e40626
Oksi598 <radovunoksana@gmail.com>
2025-02-04 21:17:46 +0200

    design for login director and worker

--- client/src/app/(authentication)/login/director/page.tsx
+++ client/src/app/(authentication)/login/director/page.tsx
@@ -1,7 +1,61 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Login Page Director</h1>
+import React from "react";
+import Image from "next/image";
+import "./style.scss";
+
+export default function DirectorLogin() {
+  return (
+    <div className="director-login-container">
+      <div className="login-form-section">
+        <Image
+          src="/logo/Logo-Dark.png"
+          alt="DOSTAVKA Logo"
+          width={120}
+          height={23}
+          className="logo"
+          priority
+        />
+
+        <h2>Login</h2>
+        <p className="welcome-text">Welcome back</p>
+
+        <button className="google-signin">
+          <Image src="/svg/google.svg" alt="Google Icon" width={20} height={20} />
+          Google
+        </button>
+
+        <div className="divider">
+          <span></span>
+          <p>або</p>
+          <span></span>
+        </div>
+
+        <form className="login-form">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="first name" />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="password" />
+
+          <div className="remember-me">
+            <input type="checkbox" id="remember" />
+            <label htmlFor="remember">Remember me</label>
+          </div>
+        </form>
+
+        <button type="submit" className="bottom-signin-button">
+          <Image src="/svg/user-add-white.svg" alt="Sign In Icon" width={16} height={16} />
+          Sign in
+        </button>
       </div>
-    );
+
+      <div className="image-section">
+        <Image
+          src="/Images/RegiserCar.jpeg"
+          alt="Container"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
 }
