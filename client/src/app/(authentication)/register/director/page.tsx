56813cdaf6b4e039c3a8b4ea7348d967efc0659e
k4rpenko <94958388+k4rpenko@users.noreply.github.com>
2025-02-04 15:36:24 +0200

    add fonts

--- client/src/app/(authentication)/register/director/page.tsx
+++ client/src/app/(authentication)/register/director/page.tsx
@@ -1,7 +1,31 @@
+import styles from './style.module.css';
+
 export default function Home() {
     return (
-      <div>
-        <h1>Login director</h1>
-      </div>
+        <div className={styles.appContainer}>
+            {/* Блок зображення */}
+            <div className={styles.appImage}>
+                <img src="/Images/RegiserCar.jpeg" alt="background" />
+            </div>
+
+            <div className={styles.appRegister}>
+                <div>
+                    <h2>Registration</h2>
+                    <p>Join us right now</p>
+                    <form>
+                        <input type="text" placeholder="Full Name" />
+                        <input type="text" placeholder="Company Name & Address" />
+                        <input type="email" placeholder="Email" />
+                        <input type="text" placeholder="Phone Number" />
+                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Confirm Password" />
+                        <label>
+                            <input type="checkbox" /> I agree to the Terms of Service and Privacy Policy
+                        </label>
+                        <button type="submit">Sign in</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
