13c0af501afa808a6714e411d094a6ca4a028718
k4rpenko <94958388+k4rpenko@users.noreply.github.com>
2025-02-06 22:57:59 +0200

    to fix all design

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import {redirect} from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
+  redirect("/home");
 
-    </div>
+  return (
+      <div>
+      </div>
   );
 }
