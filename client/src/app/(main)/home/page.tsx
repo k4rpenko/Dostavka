a115e96dfebde831c2dccdd2b422703aee8df475
k4rpenko <94958388+k4rpenko@users.noreply.github.com>
2025-02-09 10:26:39 +0200

    Update page.tsx

--- client/src/app/(main)/home/page.tsx
+++ client/src/app/(main)/home/page.tsx
@@ -2,10 +2,12 @@ import styles from './style.module.scss';
 import Home2 from "@/conponents/home/home2/page";
 import Home3 from "@/conponents/home/home3/page";
 import Home4 from "@/conponents/home/home4/page";
+import Home1 from "@/conponents/home/home1/page";
 
 export default function Home() {
     return (
         <div className={styles.appContainer}>
+            <Home1 />
             <Home2 />
             <Home3 />
             <Home4 />
