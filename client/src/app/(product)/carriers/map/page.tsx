4c8db7aa5ac7aab571fe10fa3ca1f54f53d46607
k4rpenko <94958388+k4rpenko@users.noreply.github.com>
2025-02-11 23:26:11 +0200

    add chat and map with tracking cars

--- client/src/app/(product)/carriers/map/page.tsx
+++ client/src/app/(product)/carriers/map/page.tsx
@@ -1,7 +1,14 @@
+import styles from "./style.module.scss";
+import MapModule from "@/conponents/carriers/Map/Map";
+
 
 export default function Map() {
+
     return (
-        <div>
+        <div className={styles.appContainer}>
+            <div className={styles.Map}>
+                <MapModule/>
+            </div>
         </div>
     );
 }
