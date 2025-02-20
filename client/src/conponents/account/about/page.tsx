fef1a042a1d26d200a9aa19c728acfee3b1b5fda
Oksi598 <radovunoksana@gmail.com>
2025-02-20 18:28:56 +0200

    design account

---
+++ client/src/conponents/account/about/page.tsx
@@ -0,0 +1,35 @@
+import styles from "./style.module.scss";
+
+export default function About() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.description}>
+              Our company is a reliable partner in the field of logistics and delivery, ensuring fast and safe transportation of goods across the country and beyond. We specialize in effective logistics solutions for businesses and private clients, offering a wide range of services:
+            </div>
+            <div className={styles.services}>
+                <div className={styles.serviceItem}>
+                  <span className={styles.icon}><svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00024000000000000003"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M19.7071 6.29289C20.0976 6.68342 20.0976 7.31658 19.7071 7.70711L10.4142 17C9.63316 17.7811 8.36683 17.781 7.58579 17L3.29289 12.7071C2.90237 12.3166 2.90237 11.6834 3.29289 11.2929C3.68342 10.9024 4.31658 10.9024 4.70711 11.2929L9 15.5858L18.2929 6.29289C18.6834 5.90237 19.3166 5.90237 19.7071 6.29289Z" fill="#0F1729"></path> </g></svg></span> 
+                  <strong>Freight Transportation</strong> – we transport small and large cargo of any complexity.
+                </div>
+                <div className={styles.serviceItem}>
+                  <span className={styles.icon}><svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00024000000000000003"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M19.7071 6.29289C20.0976 6.68342 20.0976 7.31658 19.7071 7.70711L10.4142 17C9.63316 17.7811 8.36683 17.781 7.58579 17L3.29289 12.7071C2.90237 12.3166 2.90237 11.6834 3.29289 11.2929C3.68342 10.9024 4.31658 10.9024 4.70711 11.2929L9 15.5858L18.2929 6.29289C18.6834 5.90237 19.3166 5.90237 19.7071 6.29289Z" fill="#0F1729"></path> </g></svg></span> 
+                  <strong>Express Delivery</strong> – fast transportation of documents, parcels, and goods in a short time.
+                </div>
+                <div className={styles.serviceItem}>
+                    <span className={styles.icon}><svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00024000000000000003"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M19.7071 6.29289C20.0976 6.68342 20.0976 7.31658 19.7071 7.70711L10.4142 17C9.63316 17.7811 8.36683 17.781 7.58579 17L3.29289 12.7071C2.90237 12.3166 2.90237 11.6834 3.29289 11.2929C3.68342 10.9024 4.31658 10.9024 4.70711 11.2929L9 15.5858L18.2929 6.29289C18.6834 5.90237 19.3166 5.90237 19.7071 6.29289Z" fill="#0F1729"></path> </g></svg></span> 
+                    <strong>International Logistics</strong> – we handle import and export of goods to various countries around the world.
+                </div>
+                <div className={styles.serviceItem}>
+                    <span className={styles.icon}><svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00024000000000000003"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M19.7071 6.29289C20.0976 6.68342 20.0976 7.31658 19.7071 7.70711L10.4142 17C9.63316 17.7811 8.36683 17.781 7.58579 17L3.29289 12.7071C2.90237 12.3166 2.90237 11.6834 3.29289 11.2929C3.68342 10.9024 4.31658 10.9024 4.70711 11.2929L9 15.5858L18.2929 6.29289C18.6834 5.90237 19.3166 5.90237 19.7071 6.29289Z" fill="#0F1729"></path> </g></svg></span> 
+                    <strong>Warehouse Services</strong> – storage, processing, and packaging of goods before shipment.
+                </div>
+            </div>
+            <div className={styles.extraInfo}>
+              We use modern fleets, innovative tracking technologies, and an individual approach to each client to ensure the highest service quality. Our professional team works 24/7, guaranteeing reliability, punctuality, and responsibility in every shipment.
+            </div>
+            <div className={styles.footer}>
+              Trust us with your logistics – we’ll deliver your cargo on time!
+            </div>
+        </div>
+    );  
+}
