507286a5e2daead0e4f22f5aaf16ba179c4410c0
k4rpenko <94958388+k4rpenko@users.noreply.github.com>
2025-02-22 23:47:15 +0200

    add Messages Server

--- servers/messager/messager/Program.cs
+++ servers/messager/messager/Program.cs
@@ -1,7 +1,11 @@
+using Hash;
+using Hash.Interface;
 using Microsoft.EntityFrameworkCore;
+using MongoDB;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetSection("Npgsql:ConnectionString").Value));
@@ -10,19 +14,56 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddSignalR();
+builder.Services.AddSignalR().AddJsonProtocol(options => { });
+
+builder.Services.AddSingleton<AppMongoContext>();
+builder.Services.AddScoped<IJwt, JWT>();
+builder.Services.AddScoped<IHASH256, HASH256>();
+builder.Services.AddScoped<IRSAHash, RSAHash>();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials());
+});
+
+
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
+app.Use(async (context, next) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (context.Request.Path.Value.Contains("@"))
+    {
+        context.Response.StatusCode = 403;
+        await context.Response.WriteAsync("Access Denied");
+        return;
+    }
 
+    context.Response.OnStarting(() =>
+    {
+        context.Response.Headers.Add("X-Frame-Options", "DENY");
+
+        context.Response.Headers.Add("Content-Security-Policy",
+            "default-src 'self'; " +
+            "script-src 'self' http://localhost:4200 https://localhost:8080 https://localhost:8081 'unsafe-inline' 'unsafe-eval'; " +
+            "connect-src 'self'; " +
+            "img-src 'self'; " +
+            "style-src 'self' 'unsafe-inline';");
+
+        return Task.CompletedTask;
+    });
+
+    await next();
+});
+
+app.UseCors("AllowSpecificOrigin");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.Run();
+await app.RunAsync();
