03f573f39c5e67233fff20fc080402ebde663618
k4rpenko <94958388+k4rpenko@users.noreply.github.com>
2025-02-22 15:16:05 +0200

    add oll server

---
+++ servers/сommunity/PgAdmin/Model/CarriersDirector.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PgAdmin.Hash;
+using System.ComponentModel.DataAnnotations;
+
+namespace PGAdminDAL.Model
+{
+    public class CarriersDirector
+    {
+        public CarriersDirector()
+        {
+            ConcurrencyStamp = new sha256().GenerateKey().ToString();
+        }
+
+
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string IdCompany { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmation { get; set; }
+        public string HashPassword { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmation { get; set; }
+        public string Location { get; set; }
+        public string Avatar { get; set; }
+        public int SuccessfulTransportation { get; set; }
+        public List<string> TruckId = new List<string>();
+        public List<string> TransportationId = new List<string>();
+        public List<string> WorkersId = new List<string>();
+        public List<string> EmployeeKeys = new List<string>();
+        public List<string> EmployeeBusKeys = new List<string>();
+        public List<string> EmployeeUsageKeys = new List<string>();
+        public string ConcurrencyStamp { get; set; }
+    }
+}
