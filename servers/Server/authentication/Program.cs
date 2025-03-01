d61a46412c9677f6f0e24d3c37bb94be951f267d
alladdiks <124471165+Alladdik@users.noreply.github.com>
2025-03-01 14:00:43 +0200

    Update Program.cs

--- servers/Server/authentication/Program.cs
+++ servers/Server/authentication/Program.cs
@@ -1,23 +1,24 @@
 using Hash;
 using Hash.Interface;
 using Microsoft.EntityFrameworkCore;
-using MongoDB;
-using System;
-using authentication.Models;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetSection("Npgsql:ConnectionString").Value));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Npgsql:ConnectionString")));
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Authentication API", Version = "v1" });
+});
 
 builder.Services.AddSignalR();
-builder.Services.AddSignalR().AddJsonProtocol(options => { });
 
-builder.Services.AddSingleton<AppMongoContext>();
+builder.Services.AddScoped<IArgon2Hasher, Argon2Hasher>();
 builder.Services.AddScoped<IJwt, JWT>();
 builder.Services.AddScoped<IHASH256, HASH256>();
 builder.Services.AddScoped<IRSAHash, RSAHash>();
@@ -25,49 +26,44 @@ builder.Services.AddScoped<IRSAHash, RSAHash>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("http://localhost:4200")
-                          .AllowAnyHeader()
-                          .AllowAnyMethod()
-                          .AllowCredentials());
+        builder => builder
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
 });
 
 var app = builder.Build();
 
-app.Use(async (context, next) =>
+app.Urls.Add("https://localhost:5001");
+app.Urls.Add("http://localhost:5000");
+
+app.UseRouting();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    if (context.Request.Path.Value?.Contains("@") == true)
-    {
-        context.Response.StatusCode = 403;
-        await context.Response.WriteAsync("Access Denied");
-        return;
-    }
-
-    context.Response.OnStarting(() =>
-    {
-        context.Response.Headers.Append("X-Frame-Options", "DENY");
-
-        context.Response.Headers.Append("Content-Security-Policy",
-            "default-src 'self'; " +
-            "script-src 'self' http://localhost:4200 https://localhost:8080 https://localhost:8081 'unsafe-inline' 'unsafe-eval'; " +
-            "connect-src 'self'; " +
-            "img-src 'self'; " +
-            "style-src 'self' 'unsafe-inline';");
-
-        return Task.CompletedTask;
-    });
-
-    await next();
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Authentication API V1");
+    c.RoutePrefix = "swagger";
 });
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseExceptionHandler("/Error");
+app.UseHsts();
+
 
 app.UseCors("AllowSpecificOrigin");
-app.UseHttpsRedirection();
+
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
+
+app.MapMethods("/api/{**any}", new[] { "OPTIONS" }, (HttpContext context) =>
+{
+    context.Response.Headers.Append("Access-Control-Allow-Origin", "http://localhost:3000");
+    context.Response.Headers.Append("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    context.Response.Headers.Append("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    return Task.CompletedTask;
+});
 
 await app.RunAsync();
