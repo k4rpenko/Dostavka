21cafa0113969968842e80f959942ec8bde7d0be
alladdiks <124471165+Alladdik@users.noreply.github.com>
2025-03-01 13:58:50 +0200

    Update Worker.cs

--- servers/Server/authentication/Models/Worker.cs
+++ servers/Server/authentication/Models/Worker.cs
@@ -1,52 +1,62 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace authentication.Models
 {
-    public class Worker
+    public class Workers
     {
-        public Worker()
+        public Workers()
         {
+            Id = Guid.NewGuid().ToString();
             ConcurrencyStamp = Guid.NewGuid().ToString();
         }
 
         [Key]
-        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Id { get; set; }
 
         [Required]
+        [MaxLength(255)]
         public string IdCompany { get; set; }
 
         [Required]
+        [MaxLength(255)]
         public string FullName { get; set; }
 
         [Required, EmailAddress]
+        [MaxLength(255)]
         public string Email { get; set; }
 
-        public bool? EmailConfirmation { get; set; }
+        public bool EmailConfirmation { get; set; } = false;
 
         [Required]
+        [MaxLength(255)]
         public string HashPassword { get; set; }
 
         [Required, Phone]
+        [MaxLength(20)]
         public string PhoneNumber { get; set; }
 
-        public bool? PhoneNumberConfirmation { get; set; }
+        public bool PhoneNumberConfirmation { get; set; } = false;
 
+        [MaxLength(500)]
         public string? Location { get; set; }
 
-        public string Avatar { get; set; }
+        [MaxLength(500)]
+        public string? Avatar { get; set; }
 
-        public int? SuccessfulTasks { get; set; }
+        public HashSet<string> AssignedTaskIds { get; set; } = new();
+        public HashSet<string> CompletedTaskIds { get; set; } = new();
 
-        public List<string>? AssignedTaskIds { get; set; } = new();
-
-        public List<string>? CompletedTaskIds { get; set; } = new();
-
+        [MaxLength(500)]
         public string? RefreshToken { get; set; }
 
-        public string? ConcurrencyStamp { get; set; }
+        [MaxLength(255)]
+        public string ConcurrencyStamp { get; set; }
 
         [Required]
+        [MaxLength(255)]
         public string CompanyKey { get; set; }
     }
 }
