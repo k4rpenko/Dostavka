c154cf826a91f2968c3fa3bb393bd41a6a2dde2c
Dima1385 <Lozik1385@gmail.com>
2025-03-04 12:21:56 +0200

    fix

--- servers/Server/main/Controllers/companies.cs
+++ servers/Server/main/Controllers/companies.cs
@@ -1,6 +1,51 @@
-﻿namespace main.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PgAdmin.Model;
+using System.Threading.Tasks;
+
+namespace main.Controllers
 {
-    public class companies
+    [ApiController]
+    [Route("api/[controller]")]
+    public class companies : Controller
     {
+        private readonly AppDbContext _context;
+
+        public companies(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCompanies()
+        {
+            var companies = await _context.Companys
+                .Where(c => c.Role == "7") 
+                .Select(c => new {
+                    c.Id,
+                    c.Title,
+                    c.Avatar,
+                    c.Rating,
+                    c.WorkersNumber,
+                    c.TransportationNumber,
+                    c.SuccessfulTransportation,
+                    c.email,
+                    c.PhoneNumber
+                })
+                .ToListAsync();
+
+            return Ok(companies);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCompanyById(string id)
+        {
+            var company = await _context.Companys.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return Ok(company);
+        }
     }
 }
